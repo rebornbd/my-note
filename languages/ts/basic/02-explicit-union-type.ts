8c48af9febdb69856e87e0b1b09f3cee27be4733
dipta <me.dipta.dhar@gmail.com>
2022-06-15 00:53:48 +0600

    explicit & union type

---
+++ languages/ts/basic/02-explicit-union-type.ts
@@ -0,0 +1,11 @@
+// Explicit Type
+const person: string = "Rahim";
+const isAdult: boolean = true;
+
+
+
+// Union Type
+let mixed: (string | number | boolean)[] = [];
+mixed.push(10);
+mixed.push("string");
+mixed.push(true);
