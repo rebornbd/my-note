78b0a24df8e86d19bc535081f0dc868a82aa5e27
dipta <me.dipta.dhar@gmail.com>
2022-06-15 01:21:21 +0600

    Function

--- languages/ts/basic/04-function.ts
+++ languages/ts/basic/04-function.ts
@@ -3,4 +3,35 @@
 */
 
 
-// 
+// function signatures
+let log: (message: string) => void;
+let add: (num1: number, num2: number) => number;
+let calculate: (num1: number, num2: number, num3?: number) => number;
+
+
+// function body
+log = (message) => {
+  console.log(message);
+}
+
+add = (num1, num2) => {
+  return num1 + num2;
+}
+
+calculate = (num1, num2, num3 = 0) => {
+  return num1 + num2 + num3;
+}
+
+const calculation = (num1: number, num2: number, num3: number = 0): number => {
+  return num1 + num2 + num3;
+}
+
+
+// function call
+log(`Hi ${"rahim"}`);
+add(10, 10);
+
+
+calculate(5, 10);
+calculation(2, 5);
+calculation(5, 3, 2);
