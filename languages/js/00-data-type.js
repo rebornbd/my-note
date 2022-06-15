5880c45bf83c1104d1e2ef722da347e56399d521
dipta <me.dipta.dhar@gmail.com>
2022-06-16 01:42:13 +0600

    data type | symbol discribe

--- languages/js/00-data-type.js
+++ languages/js/00-data-type.js
@@ -17,4 +17,37 @@
 */
 
 
-// symbol
+////////////////////
+// symbol (symbol return a unique value)
+////////////////////
+const s1 = Symbol();
+const s2 = Symbol();
+
+const symbol1 = Symbol("symbol 01"); // unique symbol
+const symbol2 = Symbol("symbol 02"); // unique symbol
+
+const symbol3 = Symbol.for("key_name"); // symbol symbol3 & symbol4 are same
+const symbol4 = Symbol.for("key_name");
+const symbol5 = Symbol.for("another_key_name");
+
+const symbolValue = symbol1.valueOf();  // Symbol('symbol 01')
+const symbolDesc = symbol1.description; // symbol 01
+const symbolStr = symbol1.toString();   // "Symbol('symbol 01')"
+
+
+////////////////////
+// symbol usecase 01: object dynamic key
+////////////////////
+const userId = Symbol("User ID");
+const userIdValue = Math.floor(Math.random()*100);
+const person = {
+  name: 'rahim',
+  age: 27,
+  [userId]: userIdValue
+};
+const personProps = Object.getOwnPropertyNames(person);
+const personSymbolProps = Object.getOwnPropertySymbols(person);
+for(const psp of personSymbolProps) {
+  const personUserIdValue = person[psp];
+  // console.log(personUserIdValue, personUserIdValue === userIdValue);
+}
