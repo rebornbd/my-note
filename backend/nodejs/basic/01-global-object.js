f18f2be5176cdcf3b23e22c448d24e0904d4f7f4
dipta <me.dipta.dhar@gmail.com>
2022-06-18 15:09:19 +0600

    global objects

---
+++ backend/nodejs/basic/01-global-object.js
@@ -0,0 +1,19 @@
+/*
+  Global Objects are the objects that are available in all modules.
+*/
+
+////////////////////
+// Global Objects
+////////////////////
+// setTimeout();
+// clearTimeout();
+// setInterval();
+// clearInterval();
+
+// global.setTimeout();
+// global.clearTimeout();
+// global.setInterval();
+// global.clearInterval();
+
+console.log(global);
+global.setTimeout(() => { console.log("RUN!"); }, 1000);
