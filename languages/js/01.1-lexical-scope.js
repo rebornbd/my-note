52ef7dc95fc33770bce9a5dc777042f694417c5b
dipta <me.dipta.dhar@gmail.com>
2022-06-20 02:29:49 +0600

    lexical scope

---
+++ languages/js/01.1-lexical-scope.js
@@ -0,0 +1,7 @@
+/*
+  Lexical Scope:
+  ==============
+  Lexical scope means that in a nested group of functions, 
+  the inner functions have access to the variables and 
+  other resources of their parent scope.
+*/
